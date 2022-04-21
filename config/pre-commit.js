189b2f5a006c648957d968ad010d76f38280a7da
yuanhao <yuanhao_zzz@163.com>
2022-04-21 11:21:38 +0800

    deploy

--- config/pre-commit.js
+++ config/pre-commit.js
@@ -10,9 +10,7 @@ function checkConflict() {
     if (item.match(/^<<<<<<<$|^=======$|^>>>>>>>\\s/)) {
     }
   });
-  // 你大爷的
-
-  // 你三大爷的
+  // 你大爷的啊啊啊
 
   // if (conflicts) {
   //   errorLog(["错误：发现冲突，请解决完后在提交", "错误代码：", conflicts]);
